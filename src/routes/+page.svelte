23a38d1d582e2ff7dfd7473affe87d96b1d8c8cc
sabrina <sabrinaabrodi20@gmail.com>
2023-04-19 13:40:23 +0200

    feat: basic layout pt 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,3 +13,14 @@
     <button style="width: 200px;">Add</button>
   </div>
 </div>
+
+<div
+  style="display: flex; align-items: baseline; width: 700px; margin: 0 auto;"
+>
+  <input type="checkbox" />
+  <h4 style="flex-grow: 1">Do something</h4>
+  <div style="display: flex">
+    <button>Edit</button>
+    <button>Delete</button>
+  </div>
+</div>
