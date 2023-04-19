5c53a506ccc3c6104d04475ce9809356543dfd7e
sabrina <sabrinaabrodi20@gmail.com>
2023-04-19 13:33:58 +0200

    feat: basic layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,12 @@
     href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css"
   />
 </svelte:head>
+
+<div style="margin: 0 auto; padding: 20px; width: 700px;">
+  <h2 style="text-align: center;">ToDo List</h2>
+  <p>Enter your ToDo here</p>
+  <div style="display: flex;">
+    <input type="text" />
+    <button style="width: 200px;">Add</button>
+  </div>
+</div>
