e90d36dad5f0226e15c1c30e2c53c692ac1a0568
sabrina <sabrinaabrodi20@gmail.com>
2023-04-21 21:36:06 +0200

    feat: adding fuctionaity and logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,23 @@
+<script>
+  let toDoList = []; // array of ToDos
+  let textInput = "";
+  function addToDo() {
+    toDoList = [
+      ...toDoList,
+      { content: textInput, editing: false, checked: false },
+    ];
+  }
+  function setEditing(i, isEditing) {
+    toDoList[i].editing = isEditing; // true / false
+  }
+  function deleteTodo(i) {
+    toDoList.splice(i, 1);
+    toDoList = toDoList;
+  }
+</script>
+
+49 lines (43 sloc) 1.57 KB
+
 <svelte:head>
   <link
     rel="stylesheet"
@@ -9,18 +29,28 @@
   <h2 style="text-align: center;">ToDo List</h2>
   <p>Enter your ToDo here</p>
   <div style="display: flex;">
-    <input type="text" />
-    <button style="width: 200px;">Add</button>
+    <input type="text" bind:value={textInput} />
+    <button style="width: 200px;" on:click={addToDo}>Add</button>
   </div>
 </div>
 
-<div
-  style="display: flex; align-items: baseline; width: 700px; margin: 0 auto;"
->
-  <input type="checkbox" />
-  <h4 style="flex-grow: 1">Do something</h4>
-  <div style="display: flex">
-    <button>Edit</button>
-    <button>Delete</button>
+{#each toDoList as toDo, i}
+  <div
+    style="display: flex; align-items: baseline; width: 700px; margin: 0 auto;"
+  >
+    {#if toDo.editing}
+      <input type="text" bind:value={toDo.content} />
+    {:else}
+      <input type="checkbox" bind:checked={toDo.checked} />
+      <h4 style="flex-grow: 1">{toDo.content}</h4>
+    {/if}
+    <div style="display: flex">
+      {#if toDo.editing}
+        <button on:click={() => setEditing(i, false)}>Save</button>
+      {:else}
+        <button on:click={() => setEditing(i, true)}>Edit</button>
+      {/if}
+      <button on:click={() => deleteTodo(i)}>Delete</button>
+    </div>
   </div>
-</div>
+{/each}
